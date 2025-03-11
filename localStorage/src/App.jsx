f81fbb2b45f82d1c863dd2f15b198cc2749d6d65
SophiaPbh <143319358+SophiaPbh@users.noreply.github.com>
2025-03-11 09:57:56 +0100

    n
    n

--- localStorage/src/App.jsx
+++ localStorage/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -10,12 +10,19 @@ import Welcome from "./components/Welcome";
 
 function App() {
   const [signedIn, setSignedIn] = useState(false);
+  const [storageUser, setStorageUser] = useState(localStorage.getItem("user"));
+
+  useEffect(() => {
+    const login = sessionStorage.getItem("login");
+    setSignedIn(login);
+    console.log("SessionStorage", Boolean(login));
+}, [signedIn]);
 
   return (
-    <Layout>
+    <Layout signedIn={signedIn} setSignedIn={setSignedIn}>
       <Routes>
-        <Route path="/" element={signedIn ? <Welcome /> : <Login />} />
-        <Route path="login" element={<Login />} />
+        <Route path="/" element={signedIn ? <Welcome /> : <Login storageUser={storageUser} setSignedIn={setSignedIn} signedIn={signedIn} />} />
+        <Route path="login" element={<Login storageUser={storageUser} setSignedIn={setSignedIn} signedIn={signedIn} />} />
         <Route path="signup" element={<Signup />} />
       </Routes>
     </Layout>
